f40bd43bb23af1054e0c5b7f92930b2b31ae1389
Ruby Marsden <connemara.marsden@gmail.com>
2021-10-18 16:22:02 +0800

    Setting up the reference material selection dialog

--- src/view/reference_material_dialog.py
+++ src/view/reference_material_dialog.py
@@ -1,1 +1,40 @@
 # Popup for reference selection
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QDialogButtonBox, QCheckBox
+from src.model.settings.isotope_reference_materials import *
+
+
+class ReferenceMaterialSelectionDialog(QDialog):
+    def __init__(self, isotope):
+        QDialog.__init__(self)
+        self.isotope = isotope
+        self.setWindowTitle("Reference material selection")
+        self.setMinimumWidth(450)
+        layout = QVBoxLayout()
+
+        title = QLabel("Select a primary reference material")
+
+        self.reference_list = QVBoxLayout()
+
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        if self.isotope == "o":
+            reference_materials = [*oxygen_reference_material_dict]
+
+        elif self.isotope == "s":
+            reference_materials = [*sulphur_reference_material_dict]
+
+        else:
+            raise Exception
+
+        for i in range(0, len(reference_materials)):
+            box = QCheckBox(reference_materials[i])
+            self.reference_list.addWidget(box)
+
+        layout.addWidget(title)
+        layout.addLayout(self.reference_list)
+        layout.addWidget(self.buttonBox)
+        self.setLayout(layout)
+
+
--- src/view/sidrs_window.py
+++ src/view/sidrs_window.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import Qt, QSize
 
 from src.view.isotope_button_widget import IsotopeButtonWidget
 from src.view.file_entry_widget import FileEntryWidget
+from src.view.reference_material_dialog import ReferenceMaterialSelectionDialog
 
 
 class SidrsWindow(QMainWindow):
@@ -19,6 +20,7 @@ class SidrsWindow(QMainWindow):
         title = QLabel("Stable Isotope Data Reduction for CAMECA data")
         title.setAlignment(Qt.AlignCenter)
         next_button = QPushButton("Next")
+        next_button.clicked.connect(self.next_button_clicked)
         main_layout = QVBoxLayout()
         main_widget.setLayout(main_layout)
 
@@ -28,3 +30,8 @@ class SidrsWindow(QMainWindow):
         main_layout.addWidget(next_button, alignment=Qt.AlignRight)
 
         return main_widget
+
+    def next_button_clicked(self):
+        isotope = "s"
+        dialog = ReferenceMaterialSelectionDialog(isotope)
+        result = dialog.exec()
