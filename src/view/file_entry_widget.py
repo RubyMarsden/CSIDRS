174f334a7c0923e4b3c39302b8e12230a06fb72a
Ruby Marsden <connemara.marsden@gmail.com>
2021-10-18 14:57:36 +0800

    Added file entry to view (but not model).

--- src/view/file_entry_widget.py
+++ src/view/file_entry_widget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton, QFileDialog
 
 
 class FileEntryWidget(QWidget):
@@ -10,4 +10,21 @@ class FileEntryWidget(QWidget):
 
         label = QLabel("File entry goes here")
 
+        self.file_entry_button = QPushButton("Select data files")
+        self.file_entry_button.clicked.connect(self.on_file_entry_button_clicked)
+
         layout.addWidget(label)
+        layout.addWidget(self.file_entry_button)
+
+        #############
+        ## Actions ##
+        #############
+
+    def on_file_entry_button_clicked(self):
+        filenames, _ = QFileDialog.getOpenFileNames()
+        # model needs to do the importing
+        # self.model.import_samples(filenames)
+        if filenames:
+            for filename in filenames:
+                print(filename)
+
