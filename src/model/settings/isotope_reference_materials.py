3c1c171fae6f32b1962ab1cb94eed79dcac5ba29
Ruby Marsden <connemara.marsden@gmail.com>
2021-10-18 16:21:38 +0800

    Created a reference material setting file.

---
+++ src/model/settings/isotope_reference_materials.py
@@ -0,0 +1,10 @@
+
+oxygen_reference_material_dict = {
+    "reference_1": "value 1",
+    "reference_2": "value 2"
+}
+
+sulphur_reference_material_dict = {
+    "s reference 1": "s value 1",
+    "s reference 2": "s value 2"
+}
