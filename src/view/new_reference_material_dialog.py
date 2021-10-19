2824d89348fd88366ea7ed6ba00671ced4366fb6
Ruby Marsden <connemara.marsden@gmail.com>
2021-10-19 10:16:43 +0800

    Added new reference material button in reference material dialog. Added new reference dialog.

---
+++ src/view/new_reference_material_dialog.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QDialog
+
+
+class NewReferenceMaterialDialog(QDialog):
+    def __init__(self, isotope):
+        QDialog.__init__(self)
+        self.isotope = isotope
+        self.setWindowTitle("Add new reference material")
+        self.setMinimumWidth(450)
+
+
+    def add_reference_material(self):
+        return
--- src/view/reference_material_dialog.py
+++ src/view/reference_material_dialog.py
@@ -1,6 +1,7 @@
 # Popup for reference selection
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QDialogButtonBox, QCheckBox
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QDialogButtonBox, QCheckBox, QPushButton
 from src.model.settings.isotope_reference_materials import *
+from src.view.new_reference_material_dialog import NewReferenceMaterialDialog
 
 
 class ReferenceMaterialSelectionDialog(QDialog):
@@ -14,6 +15,9 @@ class ReferenceMaterialSelectionDialog(QDialog):
         title = QLabel("Select a primary reference material")
 
         self.reference_list = QVBoxLayout()
+        self.new_reference_material_button = QPushButton("New reference")
+        self.new_reference_material_button.setFixedWidth(150)
+        self.new_reference_material_button.clicked.connect(self.new_reference_material_button_clicked)
 
         self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         self.buttonBox.accepted.connect(self.accept)
@@ -32,6 +36,8 @@ class ReferenceMaterialSelectionDialog(QDialog):
             box = QCheckBox(reference_materials[i])
             self.reference_list.addWidget(box)
 
+        self.reference_list.addWidget(self.new_reference_material_button)
+
         layout.addWidget(title)
         layout.addLayout(self.reference_list)
         layout.addWidget(self.buttonBox)
@@ -40,3 +46,13 @@ class ReferenceMaterialSelectionDialog(QDialog):
     def get_selected_reference_material(self):
         # TODO: Add functionality
         return
+
+    def new_reference_material_button_clicked(self):
+        # TODO: make isotope come from the model
+        isotope = "s"
+        dialog = NewReferenceMaterialDialog(isotope)
+        result = dialog.exec()
+        if result == QDialog.Accepted:
+            return dialog.add_reference_material()
+        return None
+
