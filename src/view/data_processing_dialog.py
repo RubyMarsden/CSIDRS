cb1a53eae395b6d0e042b08c14383d144de50f51
Ruby Marsden <connemara.marsden@gmail.com>
2022-05-19 09:27:42 +0800

    Halfway through adding sulphur CAP widget

--- src/view/data_processing_dialog.py
+++ src/view/data_processing_dialog.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QTabWidget
 
+from src.model.elements import Element
 from src.view.basic_data_check_widget import BasicDataCheckWidget
 from src.view.corrected_data_widget import CorrectedDataWidget
 from src.view.drift_correction_widget import DriftCorrectionWidget
 from src.view.quality_control_widget import QualityControlWidget
 from src.view.sample_tree import SampleTreeWidget
+from src.view.sulphur_CAP_widget import SulphurCAPWidget
 
 
 class DataProcessingDialog(QDialog):
@@ -16,6 +18,7 @@ class DataProcessingDialog(QDialog):
         self.sample_tree = SampleTreeWidget(self)
 
         self.method = model.method
+        self.element = model.element
 
         self.setWindowTitle("Data processing")
         self.setMinimumWidth(500)
@@ -43,6 +46,8 @@ class DataProcessingDialog(QDialog):
         self.tabs.addTab(DriftCorrectionWidget(self), "2. Drift correction")
         self.tabs.addTab(QualityControlWidget(self), "3. Quality control")
         self.tabs.addTab(CorrectedDataWidget(self), "4. Corrected data")
+        if self.element == Element.SUL:
+            self.tabs.addTab(SulphurCAPWidget(self), "5. Sulphur CAP graphs")
 
         layout = QVBoxLayout()
 
---
+++ src/view/sulphur_CAP_widget.py
@@ -0,0 +1,97 @@
+from PyQt5.QtWidgets import QWidget, QTabWidget, QVBoxLayout
+from matplotlib import pyplot as plt
+
+from src.model.isotopes import Isotope
+from src.utils import gui_utils
+
+
+class SulphurCAPWidget(QWidget):
+    def __init__(self, data_processing_dialog):
+        QWidget.__init__(self)
+
+        self.data_processing_dialog = data_processing_dialog
+        self.ratio = self.data_processing_dialog.method.ratios[0]
+        self.samples = data_processing_dialog.samples
+        self.method = data_processing_dialog,method
+
+        layout = QVBoxLayout()
+
+        graph_widget = self._create_graph_tab_widget()
+        layout.addWidget(graph_widget)
+
+        self.setLayout(layout)
+
+    def _create_graph_tab_widget(self):
+        delta_four_vs_delta_three_graph_widget = self._create_delta_four_vs_delta_three_graph_widget()
+        delta_four_vs_delta_six_graph_widget = self._create_delta_four_vs_delta_six_graph_widget()
+        cap_three_vs_delta_four_graph_widget = self._create_cap_three_vs_delta_four_graph_widget()
+        cap_three_vs_cap_six_graph_widget = self._create_cap_three_vs_cap_six_graph_widget()
+
+        graph_tab_widget = QTabWidget()
+        graph_tab_widget.addTab(delta_four_vs_delta_three_graph_widget, "delta33 vs delta34")
+        graph_tab_widget.addTab(cap_three_vs_delta_four_graph_widget, "Cap33 vs delta34")
+        if Isotope.S36 in self.data_processing_dialog.method.isotopes:
+            graph_tab_widget.addTab(delta_four_vs_delta_six_graph_widget, "delta36 vs delta34")
+            graph_tab_widget.addTab(cap_three_vs_cap_six_graph_widget, "Cap36 vs Cap33")
+
+        return graph_tab_widget
+
+    def _create_delta_four_vs_delta_three_graph_widget(self):
+        self.delta_four_vs_delta_three_fig = plt.figure()
+
+        self.delta_four_vs_delta_three_axis = self.delta_four_vs_delta_three_fig.add_subplot()
+        graph_widget, self.delta_four_vs_delta_three_canvas = gui_utils.create_figure_widget(
+            self.delta_four_vs_delta_three_fig, self)
+
+        return graph_widget
+
+    def _create_delta_four_vs_delta_six_graph_widget(self):
+        self.delta_four_vs_delta_six_fig = plt.figure()
+
+        self.delta_four_vs_delta_six_axis = self.delta_four_vs_delta_six_fig.add_subplot()
+        graph_widget, self.delta_four_vs_delta_six_canvas = gui_utils.create_figure_widget(
+            self.delta_four_vs_delta_six_fig, self)
+
+        return graph_widget
+
+    def _create_cap_three_vs_delta_four_graph_widget(self):
+        self.cap_three_vs_delta_four_fig = plt.figure()
+
+        self.cap_three_vs_delta_four_axis = self.cap_three_vs_delta_four_fig.add_subplot()
+        graph_widget, self.cap_three_vs_delta_four_canvas = gui_utils.create_figure_widget(
+            self.cap_three_vs_delta_four_fig, self)
+
+        return graph_widget
+
+    def _create_cap_three_vs_cap_six_graph_widget(self):
+        self.cap_three_vs_cap_six_fig = plt.figure()
+
+        self.cap_three_vs_cap_six_axis = self.cap_three_vs_cap_six_fig.add_subplot()
+        graph_widget, self.cap_three_vs_cap_six_canvas = gui_utils.create_figure_widget(
+            self.cap_three_vs_cap_six_fig, self)
+
+        return graph_widget
+
+################
+### PLOTTING ###
+################
+
+    def _create_delta_four_vs_delta_three_graph(self):
+        self.delta_four_vs_delta_three_axis.clear()
+        self.delta_four_vs_delta_three_axis.set_title("delta33 vs delta34")
+        self.delta_four_vs_delta_three_axis.spines['top'].set_visible(False)
+        self.delta_four_vs_delta_three_axis.spines['right'].set_visible(False)
+
+        for sample in self.samples:
+            delta_three = [spot.drift_corrected_deltas[self.method.ratios[0]] for spot in sample.spots]
+            ys = []
+            xs = []
+            dys = []
+            dxs = []
+
+    def _create_delta_four_vs_delta_six_graph(self):
+
+    def _create_cap_three_vs_delta_four_graph(self):
+
+
+    def _create_cap_three_vs_cap_six_graph(self):
